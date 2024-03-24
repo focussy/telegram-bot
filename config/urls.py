670ddc073ac78bb07b1f65cc723e3c98963bc017
mrmamongo <mrmamongo@gmail.com>
2024-03-24 20:52:39 +0300

    add deploy and webhook

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("focussy.urls"), name="api"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
