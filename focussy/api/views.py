c167d6b383e8f9f29ec5e871c03b32849f03938b
mrmamongo <mrmamongo@gmail.com>
2024-03-24 21:14:20 +0300

    fix webhook

--- focussy/api/views.py
+++ focussy/api/views.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 
+import anyio.from_thread
 from asgiref.sync import async_to_sync
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -17,5 +18,5 @@ def healthcheck(request: HttpRequest) -> HttpResponse:
 
 @csrf_exempt
 def webhook(request: HttpRequest) -> HttpResponse:
-    asyncio.run(dp.feed_raw_update(bot, json.loads(request.body.decode("utf-8"))))
+    anyio.from_thread.run(dp.feed_raw_update, bot, json.loads(request.body.decode("utf-8")))
     return HttpResponse("OK", status=status.HTTP_200_OK)
