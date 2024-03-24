be87135560b8d80ea7d3af55419ba38d725dac49
mrmamongo <mrmamongo@gmail.com>
2024-03-24 21:01:35 +0300

    add static

--- focussy/api/views.py
+++ focussy/api/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 
 from focussy.api.telegram.bot import bot, dp
@@ -10,5 +11,6 @@ def healthcheck(request: HttpRequest) -> HttpResponse:
     return HttpResponse("OK", status=status.HTTP_200_OK)
 
 
+@csrf_exempt
 async def webhook(request: HttpRequest) -> HttpResponse:
     return await dp.feed_webhook_update(bot, request.body.decode("utf-8"))
